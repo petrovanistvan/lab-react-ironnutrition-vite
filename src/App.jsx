167a3f980c386ff2f86578ff8f9796a9122d635e
Istvan Petrovan <petrovanistvan@gmail.com>
2023-02-22 12:34:17 +0100

    third iteration

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,54 @@ import { useState } from "react";
 import './App.css';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
-
+import FoodBox from "./assets/components/FoodBox";
 
 function App() {
-  
-  const [foodsState, setFoodsState] = useState(foods)
+  const [foodsState, setFoodsState] = useState(foods);
+  const [showForm, setShowForm] = useState(false);
 
-  return <div className='App'>
+  const handleToggleForm = () => {
+    setShowForm(!showForm);
+  };
 
-    {/* Display Add Food component here */}
+  const handleAddFood = (food) => {
+    setFoodsState([...foodsState, food]);
+  };
 
-    <Button> Hide Form / Add New Food </Button>
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const filteredFoods = foods.filter((food) => {
+      return food.name.toLowerCase().includes(searchTerm);
+    });
+    setFoodsState(filteredFoods);
+  };
 
-    {/* Display Search component here */}
-
-    <Divider>Food List</Divider>
-
-    <Row style={{ width: '100%', justifyContent: 'center' }}>
-      {foodsState.map((food)=>{
-        return(
-          <Col>
-      <Card
-        title={food.name}
-        style={{ width: 230, height: 300, margin: 10 }}
-      >
-        <img src={food.image} height={60} alt="food" />
-        <p>Calories: {food.calories}</p>
-        <p>Servings: {food.servings}</p>
-        <p>
-          <b>Total Calories: {food.calories * food.servings} </b> kcal
-        </p>
-        <Button type="primary"> Delete </Button>
-      </Card>
-    </Col>
-        )
-      })}
-    </Row>
-
-  </div>
+  return (
+    <div className='App'>
+      {showForm ? (
+        <div>
+          <Input placeholder="Name" />
+          <Input placeholder="Calories" />
+          <Input placeholder="Image URL" />
+          <Button onClick={handleToggleForm}>Cancel</Button>
+          <Button onClick={() => handleAddFood()}>Add Food</Button>
+        </div>
+      ) : (
+        <Button onClick={handleToggleForm}>Hide Form / Add New Food</Button>
+      )}
+      <Input placeholder="Search" onChange={handleSearch} />
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foodsState.map((food) => {
+          return (
+            <Col key={food.name}>
+              <FoodBox food={food} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
 }
-export default App
+
+export default App;
