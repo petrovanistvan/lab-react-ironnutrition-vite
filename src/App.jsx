26e63294397c76cadfd449af0677c25f88770852
Istvan Petrovan <petrovanistvan@gmail.com>
2023-02-22 15:07:06 +0100

    fourth iteration

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,20 @@ import AddFoodForm from "./assets/components/AddFoodForm";
 
 function App() {
   const [foodsState, setFoodsState] = useState(foods);
-  const [showForm, setShowForm] = useState(false);
 
-  // const handleToggleForm = () => {
-  //   setShowForm(!showForm);
-  // };
 
-  const handleAddFood = (food) => {
-    setFoodsState([...foodsState, food]);
-  };
-
-  const handleSearch = (event) => {
-    const searchTerm = event.target.value.toLowerCase();
-    const filteredFoods = foods.filter((food) => {
-      return food.name.toLowerCase().includes(searchTerm);
-    });
-    setFoodsState(filteredFoods);
-  };
+  function handleSubmit(e, newFood) {
+    e.preventDefault();
+    console.log(newFood);
+    setFoodsState([...foodsState, newFood]); 
+  }
 
   return (
     <div className='App'>
-      {/* {showForm ? (
-        <div>
-          <Input placeholder="Name" />
-          <Input placeholder="Calories" />
-          <Input placeholder="Image URL" />
-          <Button onClick={handleToggleForm}>Cancel</Button>
-          <Button onClick={() => handleAddFood()}>Add Food</Button>
-        </div>
-      ) : (
-        <Button onClick={handleToggleForm}>Hide Form / Add New Food</Button>
-      )} */}
+    
       
-      <Input placeholder="Search" onChange={handleSearch} />
+      <AddFoodForm handleSubmit = {handleSubmit}/>
+      
       <Divider>Food List</Divider>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {foodsState.map((food) => {
--- src/assets/components/AddFoodForm.jsx
+++ src/assets/components/AddFoodForm.jsx
@@ -1,39 +1,29 @@
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import {useState} from "react";
 
-function AddFoodForm({name, image, calories, servings}) {
+function AddFoodForm({handleSubmit}) {
+
+const [newFood, setNewFood] = useState({ name:"", image:"", calories:"", servings:"" })
+
+function handleChange(event) {
+    let typed = event.target.value
+    setNewFood({...newFood, [event.target.name] :typed})
+}
+function submitButton(e) {
+    handleSubmit(e, newFood);
+    setNewFood({name:"", image:"", calories:"", servings:""});
+}
     return (
         <div>
-            <form>
-            <Input value={name} type="text"/>
-            <Input value={image} type="text"/>
-            <Input value={calories} type="text"/>
-            <Input value={servings} type="text"/>
+            <form onSubmit={submitButton}>
+            <Input value={newFood.name} type="text" name="name" onChange={handleChange}/>
+            <Input value={newFood.image} type="text" name="image" onChange={handleChange}/>
+            <Input value={newFood.calories} type="text" name="calories" onChange={handleChange}/>
+            <Input value={newFood.servings} type="text" name="servings" onChange={handleChange}/>
+            <button type="submit">send stuff</button>
             </form>
         </div>
     );
   }
   
   export default AddFoodForm;
-
-//   import React from "react";
-// import Rating from "./Rating";
-
-// function DriverCard({ name, rating, img, car }) {
-
-//     return (
-//     <div className="driverCardContainer">
-//     <div className="imageDisplay">
-//         <img src={img} alt=""></img>
-//     </div>
-//     <div>
-//     <p>{name}</p>
-//     <Rating>
-//         {rating}
-//     </Rating>
-//         <p>{car.model} - {car.licensePlate}</p>
-//     </div>
-//     </div>
-//   );
-// }
-
-// export default DriverCard;
